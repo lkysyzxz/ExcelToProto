d258bd3c7cfbc3610fd4b65032f8b8cd9d17ba86
寒江雪 <lkysyzxz@outlook.com>
2025-07-09 02:40:04 +0800

    feat: 测试动态编译导出的cs文件并序列化生成Bytes Array

---
+++ TestConsole/Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System.Reflection;
+using Microsoft.CodeAnalysis.Emit;
+using System.Runtime.Loader;
+using ActorConfig;
+using Google.Protobuf;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+string actorCS = File.ReadAllText("G:\\GameProjects\\UtilTools\\ExcelToProto\\TestConsole\\Actor.cs");
+
+
+// 创建语法树
+SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(actorCS);
+
+var protobufAssembly = Assembly.GetAssembly(typeof(IMessage));
+
+
+
+// 自动加载所有程序集引用
+var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic).Select(a => MetadataReference.CreateFromFile(a.Location)).ToList();
+
+assemblies.Add(MetadataReference.CreateFromFile(protobufAssembly.Location));
+
+CSharpCompilation compilation = CSharpCompilation.Create(
+		"Actor",
+		syntaxTrees: new[] { syntaxTree },
+		references: assemblies,
+		options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+// 编译代码
+using (var ms = new MemoryStream())
+{
+	EmitResult result = compilation.Emit(ms);
+
+	if (!result.Success)
+	{
+		Console.WriteLine("编译失败：");
+		foreach (Diagnostic diagnostic in result.Diagnostics.Where(diag => diag.IsWarningAsError || diag.Severity == DiagnosticSeverity.Error))
+		{
+			Console.WriteLine($"{diagnostic.Id}: {diagnostic.GetMessage()}");
+		}
+	}
+	else
+	{
+		
+		ms.Seek(0, SeekOrigin.Begin);
+		Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+		Type actorType = assembly.GetType("ActorConfig.Actor");
+		Type actorArrayType = assembly.GetType("ActorConfig.Actor_Array");
+		var actorArrayCreator = actorArrayType.GetConstructor(new Type[] { });
+		object actorArray = actorArrayCreator.Invoke(new object[] { });
+
+		PropertyInfo configProperty = actorArrayType.GetProperty("Config");
+
+		object config = configProperty.GetValue(actorArray);
+
+		Type configType = config.GetType();
+
+		MethodInfo addMethod = configType.GetMethod("Add", new Type[] {actorType});
+
+		var actorCreator = actorType.GetConstructor(new Type[] { });
+
+		object actor = actorCreator.Invoke(new object[] { });
+
+		PropertyInfo nameInfo = actorType.GetProperty("Name");
+
+		nameInfo.GetSetMethod().Invoke(actor, new object[] { "WWT" });
+
+		addMethod.Invoke(config, new object[] { actor });
+
+		MethodInfo calculateSizeMethod = actorArrayType.GetMethod("CalculateSize", new Type[] { });
+		MethodInfo writeToMethod = actorArrayType.GetMethod("WriteTo", new Type[] {typeof(CodedOutputStream)});
+
+		int size = (int)calculateSizeMethod.Invoke(actorArray, new object[] { });
+
+		byte[] buffer = new byte[size];
+		var codedOutput = new CodedOutputStream(buffer);
+
+		writeToMethod.Invoke(actorArray, new object[] { codedOutput });
+
+		Actor_Array arr = Actor_Array.Parser.ParseFrom(buffer);
+
+		Console.Read();
+	}
+}
+
+
+Console.Read();
+
+
+MethodInfo[] GetExtensionMethods(Type extendedType, Assembly assembly)
+{
+	return assembly.GetTypes()
+	.Where(t => t.IsSealed && !t.IsGenericType && !t.IsNested)
+	.SelectMany(t => t.GetMethods(BindingFlags.Static | BindingFlags.Public))
+	.Where(m => m.IsDefined(typeof(ExtensionAttribute), false) &&
+	m.GetParameters()[0].ParameterType == extendedType)
+	.ToArray();
+}
